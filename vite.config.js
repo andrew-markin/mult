e9337840370f32ca589c0a5e7b5edb17629dbf6d
Andrew N. Markin <andrew.n.markin@gmail.com>
2026-05-05 18:57:15 +0300

    PWA support introduced

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,6 +20,39 @@ export default defineConfig({
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: fileURLToPath(new URL('./src/quasar.sass', import.meta.url))
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      includeAssets: ['favicon.svg', 'favicon.png', 'robots.txt'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ttf,woff,woff2}']
+      },
+      manifest: {
+        name: 'Mult',
+        short_name: 'Mult',
+        description: 'Multiplication trainer',
+        theme_color: '#37474f',
+        background_color: '#37474f',
+        orientation: 'portrait-primary',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any'
+          },
+          {
+            src: 'favicon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ]
+      }
     })
   ],
   resolve: {
